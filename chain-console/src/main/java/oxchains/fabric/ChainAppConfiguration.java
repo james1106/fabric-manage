557ddcc3137818b97e72dafdeed612ebd417c6b1
aiet <lovetcn@gmail.com>
2017-05-03 15:18:59 +0800

    support cors

---
+++ chain-console/src/main/java/oxchains/fabric/ChainAppConfiguration.java
@@ -0,0 +1,24 @@
+package oxchains.fabric;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author aiet
+ */
+@Configuration
+public class ChainAppConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
